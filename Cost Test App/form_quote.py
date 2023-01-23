18a3d2524f9785f67649ff892ccdeab3708c2d74
FrancoSanFlo <sanchezfranco222@gmail.com>
2023-01-23 00:23:42 -0300

    Add different fields for costs. Unfinished

--- Cost Test App/form_quote.py
+++ Cost Test App/form_quote.py
@@ -33,16 +33,77 @@ class AppFormQuote(UserControl):
                         cursor_color="black",
                         cursor_width=1,
                         cursor_height=18,
-                        color='black'
+                        color='black',
                     ),
                 ],
             ),
         )
 
+    def app_form_dropdown_field_status(self, name:str, expand:int):
+        return Container(
+            expand=expand,
+            height=45,
+            bgcolor='#EBEBEB',
+            border_radius=6,
+            padding=8,
+            content=Column(
+                spacing=1,
+                controls=[
+                    Text(
+                        value=name,
+                        size=9,
+                        color='black',
+                        weight='bold'
+                    ),
+                    Dropdown(
+                        height=20,
+                        text_size=13,
+                        content_padding=0,
+                        color='black',
+                        options=[
+                            dropdown.Option("ENVIADA"),
+                            dropdown.Option("NO ENVIADA"),
+                        ],
+                    ),
+                ],
+            ),
+        )
+
+    def app_form_dropdown_field_yesno(self, name:str, expand:int):
+        return Container(
+            expand=expand,
+            height=45,
+            bgcolor='#EBEBEB',
+            border_radius=6,
+            padding=8,
+            content=Column(
+                spacing=1,
+                controls=[
+                    Text(
+                        value=name,
+                        size=9,
+                        color='black',
+                        weight='bold'
+                    ),
+                    Dropdown(
+                        height=20,
+                        text_size=13,
+                        content_padding=0,
+                        color='black',
+                        options=[
+                            dropdown.Option("SI"),
+                            dropdown.Option("NO"),
+                        ],
+                    ),
+                ],
+            ),
+        )
+        pass
+
     def build(self):
         return Container(
             expand=True,
-            height=190,
+            height=250,
             bgcolor='white10',
             border=border.all(1, "#EBEBEB"),
             border_radius=8,
@@ -57,6 +118,27 @@ class AppFormQuote(UserControl):
                             self.app_form_input_field("Cliente", 2),
                             self.app_form_input_field("Solicitado por", 2),
                             self.app_form_input_field("Fecha solicitud", 1),
+                        ],
+                    ),
+                    Row(
+                        controls=[
+                            self.app_form_input_field("Folio", 1),
+                            self.app_form_input_field("Factoring", 3),
+                            self.app_form_input_field("Fecha factura", 1),
+                        ],
+                    ),
+                    Row(
+                        controls=[
+                            self.app_form_input_field("Descripción breve", True),
+                        ],
+                    ),
+                    Row(
+                        controls=[
+                            self.app_form_dropdown_field_status("Estado", 1),
+                            self.app_form_dropdown_field_yesno("Ganada", 1),
+                            self.app_form_dropdown_field_yesno("Entregado", 1),
+                            self.app_form_dropdown_field_yesno("Facturado", 1),
+                            self.app_form_dropdown_field_yesno("Pagado", 1),
                         ],
                     ),
                 ],
